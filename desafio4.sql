54b0aae8a2f59037b503de89b75c815dcd6c7fd0
Carina Cunha <carina.cunha13@gmail.com>
2022-10-25 20:51:02 -0300

    Requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT u.usuario AS 'usuario', 
+IF(YEAR(MAX(h.dt_reproducao)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS 'status_usuario'
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_reproducoes AS h
+ON h.usuario_id = u.usuario_id
+GROUP BY h.usuario_id
+ORDER BY u.usuario;
